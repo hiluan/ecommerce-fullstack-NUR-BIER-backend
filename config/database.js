ffc746ece61c8b89d7db9153101c367176cbf238
hiluan <apt.muenchen@gmail.com>
2022-12-02 15:50:27 -0800

    testing deployment

--- config/database.js
+++ config/database.js
@@ -1,10 +1,22 @@
-const path = require('path');
+// const path = require('path');
 
-module.exports = ({ env }) => ({
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
+const path = require("path");
+
+module.exports = () => ({
   connection: {
-    client: 'sqlite',
+    client: "sqlite",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: path.join(__dirname, "..", ".tmp/data.db"),
     },
     useNullAsDefault: true,
   },
