d7bfef274fda352390a5c4609d37ea2d90ea7ca6
hiluan <apt.muenchen@gmail.com>
2022-12-02 16:44:21 -0800

    testing deployment

--- config/plugins.js
+++ config/plugins.js
@@ -7,15 +7,28 @@ module.exports = ({ env }) => ({
         api_key: env("CLOUDINARY_KEY"),
         api_secret: env("CLOUDINARY_SECRET"),
       },
-      // use graphql on Heroku
+      // // use graphql on Heroku
+      // endpoint: "/graphql",
+      // shadowCRUD: true,
+      // playgroundAlways: true,
+      // depthLimit: 100,
+      // apolloServer: {
+      //   tracing: false,
+      //   // use graphql on Heroku ^
+      // },
+    },
+  },
+  graphql: {
+    enabled: true,
+    config: {
       endpoint: "/graphql",
       shadowCRUD: true,
       playgroundAlways: true,
-      depthLimit: 100,
+      defaultLimit: 10,
+      maxLimit: 20,
       apolloServer: {
-        tracing: false,
+        tracing: true,
       },
-      // use graphql on Heroku ^
     },
   },
 });
