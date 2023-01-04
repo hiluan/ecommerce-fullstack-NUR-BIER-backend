36e864f1ac78faac858e1dcdc090316c0ca7bf48
hiluan <apt.muenchen@gmail.com>
2023-01-03 17:24:12 -0800

    edit graphql for railway.app

--- config/plugins.js
+++ config/plugins.js
@@ -18,20 +18,8 @@ module.exports = ({ env }) => ({
       // },
     },
   },
-  // graphql: {
-  //   enabled: true,
-  //   config: {
-  //     endpoint: "/graphql",
-  //     shadowCRUD: true,
-  //     playgroundAlways: true,
-  //     defaultLimit: 10,
-  //     maxLimit: 20,
-  //     apolloServer: {
-  //       tracing: false,
-  //     },
-  //   },
-  // },
 
+  // use graphql on Railway.app
   graphql: {
     config: {
       endpoint: "/graphql",
